f266ff8c170cdd960559dd4cf89c89a47960fcf8
i7 <i7@DESKTOP-U6BE1LO>
2023-10-26 21:44:53 -0600

    Agregar archivos de proyecto.

---
+++ AnalizadorLexicogrupo9/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace AnalizadorLexicogrupo9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.RichTextBox();
+            this.txt2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cb = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(89, 155);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(598, 128);
+            this.txt1.TabIndex = 0;
+            this.txt1.Text = "";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(89, 346);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(598, 128);
+            this.txt2.TabIndex = 1;
+            this.txt2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "NUEVO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ABRIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cb
+            // 
+            this.cb.FormattingEnabled = true;
+            this.cb.Items.AddRange(new object[] {
+            "C++",
+            "Java"});
+            this.cb.Location = new System.Drawing.Point(317, 80);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(121, 21);
+            this.cb.TabIndex = 4;
+            this.cb.Text = "Elija lenguaje";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(480, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 60);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "ANALIZAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(575, 59);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 60);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "GUARDAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(672, 59);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(81, 60);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "CANCELAR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 564);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.cb);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Analizador Lexico";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txt1;
+        private System.Windows.Forms.RichTextBox txt2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cb;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ AnalizadorLexicogrupo9/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AnalizadorLexicogrupo9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void ParseCPlusPlusCode(string code)
+        {
+            // Define patrones regex para diferentes componentes
+            string keywordPattern = @"\b(while|for|if|else|return|cout|cin)\b";
+            string identifierPattern = @"[a-zA-Z_]\w*";
+            string constantPattern = @"\d+";
+            string operatorPattern = @"(\+|\-|\*|\/|\%|==|!=|<=|>=|<|>)";
+
+            // Combinar todos los patrones en uno solo
+            string combinedPattern = string.Format("({0})|({1})|({2})|({3})",
+                keywordPattern, identifierPattern, constantPattern, operatorPattern);
+
+            // Realizar coincidencias usando el patrón
+            MatchCollection matches = Regex.Matches(code, combinedPattern);
+
+            // Resaltar componentes y mostrarlos en la segunda RichTextBox
+            txt2.Clear();
+            foreach (Match match in matches)
+            {
+                if (match.Groups[1].Success) // Palabra clave
+                {
+                    txt2.SelectionColor = System.Drawing.Color.Blue;
+                    txt2.AppendText(match.Value + " = palabra reservada\n");
+                }
+                else if (match.Groups[2].Success) // Identificador
+                {
+                    txt2.SelectionColor = System.Drawing.Color.Black;
+                    txt2.AppendText(match.Value + " = identificador\n");
+                }
+                else if (match.Groups[3].Success) // Constante numérica
+                {
+                    txt2.SelectionColor = System.Drawing.Color.Red;
+                    txt2.AppendText(match.Value + " = constante numérica\n");
+                }
+                else if (match.Groups[4].Success) // Operador
+                {
+                    txt2.SelectionColor = System.Drawing.Color.Green;
+                    txt2.AppendText(match.Value + " = operador\n");
+                }
+            }
+        }
+
+        private void ParseJavaCode(string codej)
+        {
+            // Define patrones regex para diferentes componentes
+            string keywordPatternj = @"\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|if|goto|implements|import|instanceof|int|interface|long|native|new|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\b";
+            string identifierPatternj = @"[a-zA-Z_]\w*";
+            string constantPatternj = @"\b(true|false|null)\b";
+            string operatorPatternj = @"(\+|\-|\*|/|%|==|!=|<=|>=|<|>|=)";
+
+            // Combinar todos los patrones en uno solo
+            string combinedPatternj = string.Format("({0})|({1})|({2})|({3})",
+                keywordPatternj, identifierPatternj, constantPatternj, operatorPatternj);
+
+            // Realizar coincidencias usando el patrón
+            MatchCollection matchesj = Regex.Matches(codej, combinedPatternj);
+
+            // Resaltar componentes y mostrarlos en la segunda RichTextBox
+            txt2.Clear();
+            foreach (Match matchj in matchesj)
+            {
+                if (matchj.Groups[1].Success) // Palabra clave
+                {
+                    txt2.SelectionColor = System.Drawing.Color.Blue;
+                    txt2.AppendText(matchj.Value + " = palabra clave\n");
+                }
+                else if (matchj.Groups[2].Success) // Identificador
+                {
+                    txt2.SelectionColor = System.Drawing.Color.Black;
+                    txt2.AppendText(matchj.Value + " = identificador\n");
+                }
+                else if (matchj.Groups[3].Success) // Constante (true, false, null)
+                {
+                    txt2.SelectionColor = System.Drawing.Color.Red;
+                    txt2.AppendText(matchj.Value + " = constante\n");
+                }
+                else if (matchj.Groups[4].Success) // Operador
+                {
+                    txt2.SelectionColor = System.Drawing.Color.Green;
+                    txt2.AppendText(matchj.Value + " = operador\n");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cb.SelectedIndex = -1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i;
+            i = cb.SelectedIndex;
+            if (i == 0)
+            {
+                string code = txt1.Text;
+                ParseCPlusPlusCode(code);
+            }
+            else if (i==1)
+            {
+                string code = txt1.Text;
+                ParseJavaCode(code);
+            }
+            else
+            {
+                MessageBox.Show("por favor seleccione un lenguaje");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set the filter to .txt files
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+
+                // Read the contents of the selected .txt file
+                string fileContents = System.IO.File.ReadAllText(fileName);
+
+                // Load the file contents into the RichTextBox
+                txt1.Text = fileContents;
+            }
+    }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Set the default filter to .txt files
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            saveFileDialog.DefaultExt = "txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+
+                // Get the content of the RichTextBox
+                string content = txt2.Text;
+
+                // Write the content to the selected file
+                System.IO.File.WriteAllText(fileName, content);
+
+                MessageBox.Show("Archivo guardado correctamente");
+            }
+    }
+    }
+}
